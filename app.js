9faa37b49457d750447b00f7c967e44f6f6ab903
sxoinas12 <nandandor@hotmail.com>
2019-06-23 15:12:27 +0300

    Working on log In with Linkedin

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const RoutingSystem = require('./Systems/RoutingSystem')
 const UserModule = require("./routes/Users/index")
 const session = require("express-session");
 const cors = require('cors');
+//will be movded
+const rp = require('request-promise');
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(logger('dev'));
@@ -25,6 +28,29 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
+
+app.get("/auth",(req,res,next) => {
+	console.log("Loggin with linkding....")
+	let options = {
+		method:"GET",
+		uri:"https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=77yj9s749chsnc&redirect_uri=http://localhost:3001/auth/linkedin/callback&state=fooobar&scope=r_liteprofile%20r_emailaddress%20w_member_social",
+		resolveWithFullResponse:true
+	}
+	rp(options)
+	.then((response) => {
+		console.log("###########",response.headers)
+		console.log("##########",)
+		res.send(response.request.uri.href)
+	}).catch((e) => {
+		res.send(e)
+	})
+})
+
+
+app.get("/auth/linkedin/callback",(req,res,next) => {
+	console.log("redirected i guesss but shall be front end")
+	res.send("redirected")
+})
 
 //app.use(UserModule._middleware);
 app.use(RoutingSystem.router);
